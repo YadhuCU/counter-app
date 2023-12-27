94069b5ce884b3445e2cde8d5192e9046a98257b
YadhuCU <yadhuakku003@gmail.com>
2023-12-27 12:47:06 +0530

    added incrementByAmount option

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,9 +1,24 @@
 import { useDispatch, useSelector } from "react-redux";
-import { decrement, increment, reset } from "./redux/countSlice";
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+  reset,
+} from "./redux/countSlice";
+import { useState } from "react";
 
 export const Counter = () => {
+  const [amount, setAmount] = useState("");
   const dispatch = useDispatch();
+
   const count = useSelector((state) => state.counterReducer.count);
+  const handleChange = () => {
+    if (!amount) alert("Input field should not be empty.");
+    else {
+      dispatch(incrementByAmount(Number(amount)));
+      setAmount("");
+    }
+  };
   return (
     <div className="w-full md:w-1/2 rounded-xl bg-gradient-to-r from-indiogo-950 to-purple-950 shadow-lg shadow-indigo-500/50 py-20">
       <h1 className="text-8xl text-center">{count}</h1>
@@ -35,6 +50,24 @@ export const Counter = () => {
           onClick={() => dispatch(increment())}
         >
           Increment
+        </button>
+      </div>
+      <div className="flex px-5 justify-center gap-4">
+        <input
+          className=" bg-transparent caret-indigo-600 "
+          type="text"
+          placeholder="Input Increment Amount"
+          onChange={(e) => setAmount(e.target.value)}
+          value={amount}
+        />
+        <button
+          className="rounded-md bg-gradient-to-r 
+          from-indigo-700 hover:from-indigo-600
+          to-violet-950 hover:to-violet-800 
+          cursor-pointer py-3 px-7 font-bold"
+          onClick={handleChange}
+        >
+          Increment by Amount
         </button>
       </div>
     </div>
